7453231de9df58eac4d4515ebab86d946db5b705
Daniel Marbach <daniel.marbach@openplace.net>
2014-05-26 16:34:14 +0200

    GitVersion for versioning

--- Orleans.StorageProvider.RavenDB.TestInterfaces/Properties/AssemblyInfo.cs
+++ Orleans.StorageProvider.RavenDB.TestInterfaces/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("85d6129a-8b5a-4e30-bbdf-3a33482cec32")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
